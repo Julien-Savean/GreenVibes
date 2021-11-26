6d3d5223d66b4bf60c545d32edc656071b8b3f49
Julien Savéan <j.savean@gmail.com>
2021-11-26 11:32:46 +0100

    update database

--- src/Model/PlantManager.php
+++ src/Model/PlantManager.php
@@ -15,10 +15,11 @@ class PlantManager extends AbstractManager
     {
         return $this->selectOneById($id);
     }
-    
+
     public function insert(array $plants): void
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `picture`) VALUES (:name, :picutre)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `picture`) 
+        VALUES (:name, :picture)");
         $statement->bindValue(':name', $plants['name'], \PDO::PARAM_STR);
         $statement->bindValue(':picture', $plants['picture'], \PDO::PARAM_STR);
         $statement->execute();
